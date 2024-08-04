5a89962e6deb89deab3edb34af3ac87fdc933fe2
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-04 08:37:16 +0200

    added header copyright comments

--- Jour-05-VCL/1-PlusMoinsLabel/Unit1.pas
+++ Jour-05-VCL/1-PlusMoinsLabel/Unit1.pas
@@ -1,3 +1,39 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Apprendre la programmation avec Delphi
+///
+/// Copyright 2020-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Projets Delphi d'exemples développés pendant la formation en ligne gratuite pour apprendre à programmer en utilisant le langage Pascal dans Delphi réalisée en direct entre le 23 mars 2020 et le 5 mai 2020 durant le premier confinement quasi mondial lié à la pandémie du virus COVID-19.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+///      Patrick PREMARTIN
+///
+/// Site :
+///      https://apprendre-delphi.fr/apprendre-la-programmation-avec-delphi-2020.php
+///
+/// Project site :
+///      https://github.com/DeveloppeurPascal/Apprendre-la-programmation-avec-Delphi
+///
+/// ***************************************************************************
+/// File last update : 30/05/2024 20:10:12
+/// Signature : ec4e0ff79e842ca5e02f47b0694e678432cc3988
+/// ***************************************************************************
+/// </summary>
+
 unit Unit1;
 
 interface
