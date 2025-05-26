8dedea657dbc108912d9fdab035228e15c306526
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-26 16:38:35 +0200

    added or updated C2PP comments

--- Jour-18-FMX-Graphismes/2-Animations/Unit1.pas
+++ Jour-18-FMX-Graphismes/2-Animations/Unit1.pas
@@ -1,38 +1,38 @@
-/// <summary>
-/// ***************************************************************************
-///
-/// Apprendre la programmation avec Delphi
-///
-/// Copyright 2020-2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Projets Delphi d'exemples développés pendant la formation en ligne gratuite pour apprendre à programmer en utilisant le langage Pascal dans Delphi réalisée en direct entre le 23 mars 2020 et le 5 mai 2020 durant le premier confinement quasi mondial lié à la pandémie du virus COVID-19.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-///      Patrick PREMARTIN
-///
-/// Site :
-///      https://apprendre-delphi.fr/apprendre-la-programmation-avec-delphi-2020.php
-///
-/// Project site :
-///      https://github.com/DeveloppeurPascal/Apprendre-la-programmation-avec-Delphi
-///
-/// ***************************************************************************
-/// File last update : 30/05/2024 20:10:12
-/// Signature : f1ba5a13f0b0ff039df10c9ac0bdc8aef4a9efea
-/// ***************************************************************************
-/// </summary>
+(* C2PP
+  ***************************************************************************
+
+  Apprendre la programmation avec Delphi
+
+  Copyright 2020-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Projets Delphi d'exemples développés pendant la formation en ligne gratuite pour apprendre à programmer en utilisant le langage Pascal dans Delphi réalisée en direct entre le 23 mars 2020 et le 5 mai 2020 durant le premier confinement quasi mondial lié à la pandémie du virus COVID-19.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://apprendre-delphi.fr/apprendre-la-programmation-avec-delphi-2020.php
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Apprendre-la-programmation-avec-Delphi
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:12:31.546+01:00
+  Signature : f7d24d66bbc53ec4bd4715db34c7d817d7cbbc01
+  ***************************************************************************
+*)
 
 unit Unit1;
 
--- Jour-20-JeuDuPendu-FMX-FD-LB/2-FMX-FD/Project1.dpr
+++ Jour-20-JeuDuPendu-FMX-FD-LB/2-FMX-FD/Project1.dpr
@@ -1,38 +1,38 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// Apprendre la programmation avec Delphi
-///
-/// Copyright 2020-2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Projets Delphi d'exemples développés pendant la formation en ligne gratuite pour apprendre à programmer en utilisant le langage Pascal dans Delphi réalisée en direct entre le 23 mars 2020 et le 5 mai 2020 durant le premier confinement quasi mondial lié à la pandémie du virus COVID-19.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-///      Patrick PREMARTIN
-///
-/// Site :
-///      https://apprendre-delphi.fr/apprendre-la-programmation-avec-delphi-2020.php
-///
-/// Project site :
-///      https://github.com/DeveloppeurPascal/Apprendre-la-programmation-avec-Delphi
-///
-/// ***************************************************************************
-/// File last update : 30/05/2024 20:10:12
-/// Signature : 2aac71b9588ccab5c5a3996f61944f2f067e6011
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  Apprendre la programmation avec Delphi
+
+  Copyright 2020-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Projets Delphi d'exemples développés pendant la formation en ligne gratuite pour apprendre à programmer en utilisant le langage Pascal dans Delphi réalisée en direct entre le 23 mars 2020 et le 5 mai 2020 durant le premier confinement quasi mondial lié à la pandémie du virus COVID-19.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://apprendre-delphi.fr/apprendre-la-programmation-avec-delphi-2020.php
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Apprendre-la-programmation-avec-Delphi
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:12:31.673+01:00
+  Signature : 6ff8e111357164cf785b7fa2dc996a3a57e3f9f3
+  ***************************************************************************
+*)
 
 program Project1;
 
